ec3af1fecb763599c1b5aaf7fb1bd56c37e6f60f
andrei_kononov <Andrecononoff@yandex.ru>
2024-07-19 10:53:57 +0300

    Изменен и перенесен конфигурационный файл.

--- src/main/java/searchengine/repository/SiteRepository.java
+++
@@ -1,9 +0,0 @@
-package searchengine.repository;
-
-import org.springframework.stereotype.Repository;
-import searchengine.config.Site;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@Repository
-public interface SiteRepository extends JpaRepository<Site,Long>{
-}
