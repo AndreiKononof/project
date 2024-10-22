7905956c0aa9356a7c2da70e50d2d752d8998f0a
Andrei Kononov <Andrecononoff@yandex.ru>
2024-10-22 17:34:57 +0300

    Добавлены ключи для внешних таблиц.

--- src/main/java/searchengine/services/IndexServiceImpl.java
+++ src/main/java/searchengine/services/IndexServiceImpl.java
@@ -71,7 +71,6 @@ public class IndexServiceImpl implements IndexService{
             service.submit(() -> {
                 threads.add(Thread.currentThread());
                 indexingSait(site);
-
             });
         }
         service.shutdown();
